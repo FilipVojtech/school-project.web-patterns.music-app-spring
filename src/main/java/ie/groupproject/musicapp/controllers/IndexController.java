d3863a55f8323815a07c697d5e334756bc686c0e
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-12-08 14:37:56 +0000

    Add index controller

---
+++ src/main/java/ie/groupproject/musicapp/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package ie.groupproject.musicapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String rootPage(){
+        return "pages/index";
+    }
+}
