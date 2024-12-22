67fcbf86c559d3446d7f8b4d6555b1b6246b0fff
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-12-22 12:06:07 +0000

    Add Tailwind plugins

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -1,9 +1,16 @@
+import formsPlugin from "@tailwindcss/forms";
+import typographyPlugin from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [ "../resources/templates/**/*.{html,js}" ],
     theme: {
-        extend: {},
+        extend: {
+        },
     },
-    plugins: [],
+    plugins: [
+        formsPlugin,
+        typographyPlugin,
+    ],
 }
 
