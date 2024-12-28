a1b71a7a51b8961af973087fa8288310d99e0211
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2024-12-28 16:54:43 +0000

    Make directory name lowercase

--- src/main/java/ie/groupproject/musicapp/config/LocaleConfig.java
+++ src/main/java/ie/groupproject/musicapp/config/LocaleConfig.java
@@ -1,4 +1,4 @@
-package ie.groupproject.musicapp.Config;
+package ie.groupproject.musicapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
