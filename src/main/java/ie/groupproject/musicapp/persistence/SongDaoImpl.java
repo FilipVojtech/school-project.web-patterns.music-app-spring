d210add84d5902e03db075d33b9aa7c93865a655
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-01-02 19:23:47 +0000

    Fix search failing

--- src/main/java/ie/groupproject/musicapp/persistence/SongDaoImpl.java
+++ src/main/java/ie/groupproject/musicapp/persistence/SongDaoImpl.java
@@ -67,10 +67,13 @@ public class SongDaoImpl extends MySQLDao implements SongDao {
     public List<Song> searchSongs(String keyword) {
         List<Song> songs = new ArrayList<>();
         Connection conn = super.getConnection();
-        String sql = "SELECT s.* FROM songs s " +
-                "JOIN albums a ON s.album_id = a.id " +
-                "JOIN artists ar ON a.artist_id = ar.id " +
-                "WHERE s.title LIKE ? OR a.title LIKE ? OR ar.name LIKE ?";
+        String sql = "SELECT s.* " +
+                     "FROM song s " +
+                     "     JOIN music_app.album a ON s.artist_id = a.artist_id " +
+                     "     JOIN music_app.artist ar ON ar.id = a.artist_id " +
+                     "WHERE s.title LIKE ? " +
+                     "   OR a.title LIKE ? " +
+                     "   OR ar.name LIKE ?;";
 
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
             String searchPattern = "%" + keyword + "%";
@@ -84,7 +87,7 @@ public class SongDaoImpl extends MySQLDao implements SongDao {
                 Song song = new Song();
                 song.setId(resultSet.getInt("id"));
                 song.setTitle(resultSet.getString("title"));
-                song.setAlbumId(resultSet.getInt("album_id"));
+                song.setArtist_id(resultSet.getInt("artist_id"));
                 songs.add(song); // Add the song to the list
             }
         } catch (SQLException e) {
