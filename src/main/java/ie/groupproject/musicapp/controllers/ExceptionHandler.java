c4d907bf0ad4f57695506a51a7cf7b9adc9c1b78
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-01-06 18:55:33 +0000

    Add JavaDocs to Exception Handler

--- src/main/java/ie/groupproject/musicapp/controllers/ExceptionHandler.java
+++ src/main/java/ie/groupproject/musicapp/controllers/ExceptionHandler.java
@@ -23,16 +23,10 @@ public class ExceptionHandler {
         this.messageSource = messageSource;
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = NullPointerException.class)
-    public String nullPointerHandler(Model model, Locale locale) {
-        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        model.addAttribute("errorType", messageSource.getMessage("error.500", null, locale));
-        model.addAttribute("error", messageSource.getMessage("error.500Message", null, locale));
-        model.addAttribute("code", "500");
-        log.error("Null pointer exception on '{}'", request.getRequestURL());
-        return "pages/error";
-    }
-
+    /**
+     * Page not found handler.
+     * Returns a 404 with information that a page could not be found.
+     */
     @org.springframework.web.bind.annotation.ExceptionHandler(value = NoResourceFoundException.class)
     public String pageNotFoundHandler(Model model, Locale locale) {
         response.setStatus(HttpServletResponse.SC_NOT_FOUND);
@@ -43,12 +37,20 @@ public class ExceptionHandler {
         return "pages/error";
     }
 
+    /**
+     * Null Pointer Exception handler.
+     * Returns generic code 500.
+     */
     @org.springframework.web.bind.annotation.ExceptionHandler(value = NullPointerException.class)
     public String nullPointerHandler(Model model, Locale locale) {
         log.error("Null pointer exception on '{}'", request.getRequestURL());
         return return500(model, locale);
     }
 
+    /**
+     * Other exception handlers.
+     * Returns generic code 500.
+     */
     @org.springframework.web.bind.annotation.ExceptionHandler(value = Exception.class)
     public String allOtherExceptionHandler(Model model, Exception ex, Locale locale) {
         log.error("An exception happened at '{}' with message '{}'\n{}", request.getRequestURL(), ex.getMessage(), ex.getStackTrace());
